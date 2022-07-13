b32cb78a3d4e5e0969cccc145e48f6f8dc68a7ac
iknlaxmi <45654123+iknlaxmi@users.noreply.github.com>
2022-07-13 17:44:07 +0530

    Add files via upload
    Initial commit

---
+++ src/Components/contact/Contact.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import "./contact.css"
+import {MdOutlineEmail} from "react-icons/md"
+import {RiMessengerLine} from "react-icons/ri"
+import {BsWhatsapp} from "react-icons/bs"
+import { useRef } from 'react'
+import emailjs from 'emailjs-com';
+
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_6z7wuqq', 'template_50iwcn7', form.current, '3rSK5x93Mxrxz1kyX')      
+      e.target.reset()
+  };
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+          <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>laxmimit@gmail.com</h5>
+            <a href='mailto:laxmimit@gmail.com' target="_blank">Send a message</a>
+          </article>
+          <article className='contact__option'>
+          <RiMessengerLine className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>https://www.facebook.com/</h5>
+            <a href='https://www.facebook.com/' target="_blank">Send a message</a>
+          </article>
+          <article className='contact__option'>
+          <BsWhatsapp className='contact__option-icon'/>
+            <h4>WhatsApp</h4>
+            <h5>+123456789</h5>
+            <a href='https://api.whatsapp.com/send?phone=9920927706'  target="_blank">Send a message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required/>
+          <input type="text" name='email' placeholder='Your Email' required/>
+          <textarea name='message' rows="7" placeholder='Your Message' required></textarea>
+          <button type="submit" className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
